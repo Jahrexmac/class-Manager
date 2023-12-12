36573afb13d2a7a1932948ecfaefd143bd661b4f
Rorobi Anthony <Jahrexmac@gmail.com>
2023-12-12 22:38:40 +0100

    changes

--- src/pages/registration/Register.js
+++ src/pages/registration/Register.js
@@ -2,6 +2,7 @@ export default async function register(authDetail){
     const requestOptions = {
         method: "POST",
         headers: {"content-Type": "application/json"},
+        
         body: JSON.stringify(authDetail)
     }  
     const response = await fetch(`${process.env.REACT_APP_HOST}/register`, requestOptions);
